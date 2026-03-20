24a314aa4c671454b2701131d44bc2747fb642dc
jasonxue <103628493+jasonxue1@users.noreply.github.com>
2026-03-20 20:56:21 +0800

    Update windows version-information

--- gui/build.rs
+++ gui/build.rs
@@ -1,7 +1,12 @@
 fn main() {
+    println!("cargo:rerun-if-changed=../assets/icon.ico");
+
     if std::env::var_os("CARGO_CFG_TARGET_OS").as_deref() == Some(std::ffi::OsStr::new("windows")) {
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("../icons/icon.ico");
+        res.set_icon("../assets/icon.ico");
+        res.set("ProductName", "Pearl Calculator");
+        res.set("FileDescription", "A simple Pearl Calculator");
+        res.set("Comments", "A simple Pearl Calculato built with egui");
         if let Err(err) = res.compile() {
             panic!("failed to compile Windows resources: {err}");
         }
